81701bfc084d527a58c127ae5b6d50ff3d19dcdd
chrharr1 <chrharr1@uat.edu>
2020-01-30 14:16:42 -0700

    needs imp

--- Skynet HK-Aerial/Source1.cpp
+++ Skynet HK-Aerial/Source1.cpp
@@ -11,11 +11,30 @@ int main()
 	srand(static_cast<unsigned int>(time(0)));
 	//seed random number generator
 //random number between 1 and 64
-	int searchGridLowNumber = 1;
-	int searchGridHighNumber = 64;
-	int actualTargetLocation = rand() % searchGridHighNumber + searchGridLowNumber;
+	int human_searchGridLowNumber = 1;
+	int human_searchGridHighNumber = 64;
+	
+	int AI_binary_searchGridLowNumber = 1;
+	int AI_binary_searchGridHighNumber = 64;
+	
+	int AI_linear_searchGridLowNumber = 1;
+	int AI_linear_searchGridHighNumber = 64;
+	
+	int AI_random_searchGridLowNumber = 1;
+	int AI_random_searchGridHighNumber = 64;
+	
+	int human_Number_of_predictions = 0;
+	int human_Current_Location_prediction = 0;
+	
+	int AI_Linear_Number_of_predictions = 0;
+	int AI_Linear_Current_Location_prediction = 0;
+
+
+	
+	
+	int actualTargetLocation = rand() % AI_binary_searchGridHighNumber + AI_binary_searchGridLowNumber ;
 	int tries = 0;
-	int attemptToLocateTarget = 0;
+	int Ai_Linear_Current_Location_prediction = 0;
 
 	cout << "\tWelcome to AI target finder.\n\n";
 
@@ -23,27 +42,33 @@ int main()
 	{
 		// This is where the AI makes a target location guess
 		cout << "\nThe real target location is at " << actualTargetLocation << endl;
-		cout << "\nThe AI guesses the target is at location: " << attemptToLocateTarget << endl;
-		attemptToLocateTarget = ((searchGridHighNumber - searchGridLowNumber) / 2) + searchGridLowNumber;
-		++tries;
+		AI_Linear_Number_of_predictions++;
+		
+		cout << "\nThe AI guesses the target is at location: " << Ai_Linear_Current_Location_prediction << endl;
+		
+		Ai_Linear_Current_Location_prediction = ((AI_binary_searchGridHighNumber - AI_binary_searchGridLowNumber) / 2) + AI_binary_searchGridLowNumber;
+		++AI_Linear_Number_of_predictions;
 
-		if (attemptToLocateTarget > actualTargetLocation)
+		if (Ai_Linear_Current_Location_prediction > actualTargetLocation)
 			{
 				cout << "The AI target prediction was Too high!\n\n";
-				searchGridHighNumber - attemptToLocateTarget - 1;
+				AI_binary_searchGridHighNumber - Ai_Linear_Current_Location_prediction - 1;
 			}
-		else if (attemptToLocateTarget < actualTargetLocation)
+		else if (Ai_Linear_Current_Location_prediction < actualTargetLocation)
 		
 		{
 			cout << "The AI prediction was Too low!\n\n";
-			searchGridLowNumber - attemptToLocateTarget + 1;
+			AI_binary_searchGridLowNumber - Ai_Linear_Current_Location_prediction + 1;
 		}
 		else
 		{
-			cout << "\nThat's it! The AI got it in " << tries << " guesses!\n";
+			cout << "\nThat's it! The AI got it in " << human_Number_of_predictions << " guesses!\n";
 		}
-		} while (attemptToLocateTarget != actualTargetLocation);
-
+		} while (Ai_Linear_Current_Location_prediction != actualTargetLocation);
+	
+		cout << "Binary is finished. Launch the linear Ai";
+		system("pause");
 		return 0;
 
 	}
+
